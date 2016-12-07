b9ec2b5744ed84771f2e9e76b77b238884cbf9d0
Divya Manohar <dmanohar@ooyala.com>
2016-12-07 09:18:57 +0530

    removed sleep.

--- src/test/java/com/ooyala/playback/amf/PlaybackPreRollAdsTests.java
+++ src/test/java/com/ooyala/playback/amf/PlaybackPreRollAdsTests.java
@@ -31,7 +31,6 @@ public class PlaybackPreRollAdsTests extends PlaybackWebTest {
 			driver.get(url);
 
 			result = result && playValidator.waitForPage();
-			Thread.sleep(2000);
 
 			injectScript();
 
