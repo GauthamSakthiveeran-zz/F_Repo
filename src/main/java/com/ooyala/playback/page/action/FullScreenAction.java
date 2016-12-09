8ceac7105f7968f9ad419869fa8529b8b4240c1e
Jitendra Pisal <jitendra.pisal@forgeahead.io>
2016-12-09 14:31:28 +0530

    changed Fullscreen validator

--- src/main/java/com/ooyala/playback/page/action/FullScreenAction.java
+++ src/main/java/com/ooyala/playback/page/action/FullScreenAction.java
@@ -38,7 +38,9 @@ public class FullScreenAction extends PlayBackPage implements PlayerAction {
 				|| getBrowser().equalsIgnoreCase("internet explorer") || getPlatform()
 				.equalsIgnoreCase("Android"))) {
 
-			if (!waitOnElement(By.id("fullscreenChanged_true"), 60000)) {
+			Thread.sleep(5000);
+
+			if (!isElementPresent(By.id("fullscreenChanged_true"))) {
 				return false;
 			}
 
