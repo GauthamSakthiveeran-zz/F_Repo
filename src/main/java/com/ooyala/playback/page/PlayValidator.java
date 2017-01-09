e7b203ebe954ea84ce6c127e9f1a30753907f2bf
Divya Manohar <dmanohar@ooyala.com>
2017-01-09 15:55:12 +0530

    added sleep as loading spinner does not make sense here

--- src/main/java/com/ooyala/playback/page/PlayValidator.java
+++ src/main/java/com/ooyala/playback/page/PlayValidator.java
@@ -36,9 +36,9 @@ public class PlayValidator extends PlayBackPage implements PlaybackValidator {
 		boolean errorScreen = false;
 
 		try {
-			
+			System.out.println("***********@@@@@@@@@@@@ " + driver.getSessionId().toString());
 			if(waitOnElement("PLAYER_SKIN", 90000)){ // to avoid waiting for a long time for the play button to appear in case of error scenarios.
-				loadingSpinner();
+				Thread.sleep(2000);
 				if(isElementPresent("PLAY_BUTTON")){
 					extentTest.log(LogStatus.PASS, "Play button found.");
 					return true;
