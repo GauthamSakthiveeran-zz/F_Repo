d55f5a49b4c00c592153913aee7c8cb1e2a9b9ca
Divya Manohar <dmanohar@ooyala.com>
2017-01-13 13:55:32 +0530

    retries and re-enabling all tests.

--- src/main/java/com/ooyala/playback/report/ExtentManager.java
+++ src/main/java/com/ooyala/playback/report/ExtentManager.java
@@ -3,12 +3,16 @@ package com.ooyala.playback.report;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class ExtentManager {
 
 	private static Map<String, ExtentTest> extentTestMap;
 	private static PBWExtentReports extentReports;
+	private static Logger logger = Logger.getLogger(ExtentManager.class);
 
 	static {
 		extentTestMap = new HashMap<String, ExtentTest>();
@@ -36,14 +40,18 @@ public class ExtentManager {
 	}
 
 	public static synchronized ExtentTest startTest(String testName) {
-
+		boolean retry = false;
 		ExtentTest test = extentTestMap.get(testName);
 		if (test != null) {
 			extentReports.removeTest(test);
 			extentTestMap.remove(test);
+			logger.info(testName+ " is being removed from the report as it is being retried.");
+			retry = true;
 		}
-
+		
 		test = extentReports.startTest(testName);
+		if(retry)
+			test.log(LogStatus.INFO, "Test being retried.");
 		extentTestMap.put(testName, test);
 		return test;
 	}
