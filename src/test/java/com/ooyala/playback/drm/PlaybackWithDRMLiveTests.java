db7ef8cf0435e5d48cf263e5ce34c14466b65eef
Suraj Jogdand <suraj@vpn-client-229.sv2>
2017-03-30 16:23:23 +0530

    Added separate test for all DRM+live assets

---
+++ src/test/java/com/ooyala/playback/drm/PlaybackWithDRMLiveTests.java
@@ -0,0 +1,88 @@
+package com.ooyala.playback.drm;
+
+import com.ooyala.playback.PlaybackWebTest;
+import com.ooyala.playback.page.*;
+import com.ooyala.playback.page.action.SeekAction;
+import com.ooyala.playback.url.UrlObject;
+import com.ooyala.qe.common.exception.OoyalaException;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriverException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PlaybackWithDRMLiveTests extends PlaybackWebTest {
+
+	private static Logger logger = Logger.getLogger(PlaybackWithDRMLiveTests.class);
+	private EventValidator eventValidator;
+	private PlayValidator play;
+	private PauseValidator pause;
+	private SeekValidator seek;
+	private SeekAction seekAction;
+	private DRMValidator drm;
+
+	public PlaybackWithDRMLiveTests() throws OoyalaException {
+		super();
+	}
+
+	@Test(groups = "drm", dataProvider = "testUrls")
+	public void testPlaybackWithDRMLive(String testName, UrlObject url)
+			throws OoyalaException {
+		boolean result = true;
+
+		logger.info("Test Description :\n"
+				+ testName.split("-")[1].toLowerCase());
+
+		try {
+			driver.get(url.getUrl());
+
+			// need to add logic for verifying description
+			
+			boolean flag = true;
+			
+			while(flag){
+				logger.info("waiting on message bus");
+				try{
+					injectScript();
+					flag = false;
+				}catch(WebDriverException ex){
+					if(ex.getMessage().contains("unknown error: Cannot read property 'mb' of undefined")){
+						flag = true;
+					} else{
+						flag = false;
+					}
+				}
+					
+			}
+			
+			result = result && drm.validate("drm_tag", 5000);
+			
+			result = result && play.waitForPage();
+			
+			result = result && play.validate("playing_1", 60000);
+			
+			result = result && eventValidator.loadingSpinner();
+
+			Thread.sleep(2000);
+
+			result = result && pause.validate("paused_1", 60000);
+
+			result = result && play.validate("playing_2", 60000);
+
+			if (!(testName.split("-")[1].trim()
+					.equalsIgnoreCase("elemental fairplay fairplay hls + opt")))
+				result = result && seek.validate("seeked_1", 60000);
+			else{
+				result = result && seekAction.fromLast().setTime(2).startAction();
+				result = result && eventValidator.validate("seeked_1", 60000);
+			}
+
+			result = result && eventValidator.validate("played_1", 60000);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = false;
+		}
+
+		Assert.assertTrue(result, "DRM tests failed : " + testName);
+	}
+}
