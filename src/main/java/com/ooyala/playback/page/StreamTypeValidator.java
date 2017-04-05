58a48bf147666b042519d5c4bb4a39e140c3842f
Suraj Jogdand <suraj@vpn-client-70.sv2>
2017-04-05 12:19:54 +0530

    Added code for stream type mp4 validation

--- src/main/java/com/ooyala/playback/page/StreamTypeValidator.java
+++ src/main/java/com/ooyala/playback/page/StreamTypeValidator.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.support.PageFactory;
 
 import com.relevantcodes.extentreports.LogStatus;
 
+import java.util.ArrayList;
+
 /**
  * Created by suraj on 3/23/17.
  */
@@ -26,8 +28,29 @@ public class StreamTypeValidator extends PlayBackPage implements PlaybackValidat
 	}
 
 	public boolean validate(String element, int timeout) throws Exception {
-		
-		if(streamType.contains("mp4")) return true; // TODO for mp4
+
+        if(streamType.contains("mp4")){
+            String mp4Url = driver.findElementById(element).getText();
+            driver.executeScript("window.open('"+mp4Url+"')");
+            ArrayList<String> tabs = new ArrayList<String>(
+                    driver.getWindowHandles());
+            Thread.sleep(2000);
+            driver.switchTo().window(tabs.get(1));
+            Thread.sleep(2000);
+            waitOnElement(By.xpath(".//*[@type='video/mp4']"),20000);
+            String isMp4 = driver.findElement(By.xpath(".//*[@type='video/mp4']")).getAttribute("type");
+            driver.close();
+            Thread.sleep(2000);
+            driver.switchTo().window(tabs.get(0));
+            Thread.sleep(2000);
+            if (isMp4.contains("mp4")){
+                return true;
+            } else {
+                logger.info("Stream is not matching as per expected result " + streamType);
+                extentTest.log(LogStatus.PASS, "Stream is not matching as per expected result " + streamType);
+                return false;
+            }
+        }
 		
 		String streamContains = driver.findElement(By.id("videoPlayingurl")).getText();
 		
