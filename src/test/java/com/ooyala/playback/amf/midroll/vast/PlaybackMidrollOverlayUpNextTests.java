bc89c2a9638e5ea88496b20f1040686f042ce528
Jitendra Pisal <jitendra.pisal@forgeahead.io>
2017-04-11 19:17:53 +0530

    verified overlayRendring event for PlaybackMidrollOverlayUpNextTests test

--- src/test/java/com/ooyala/playback/amf/midroll/vast/PlaybackMidrollOverlayUpNextTests.java
+++ src/test/java/com/ooyala/playback/amf/midroll/vast/PlaybackMidrollOverlayUpNextTests.java
@@ -19,17 +19,13 @@ public class PlaybackMidrollOverlayUpNextTests extends PlaybackWebTest {
 
 	private EventValidator event;
 	private PlayValidator playValidator;
-	private OverlayValidator overLayValidator;
+	private OverlayValidator overlayValidator;
 	private DiscoveryValidator discoverValidator;
 
 	@Test(groups = {"amf","overlay","midroll","upnext","sequential"}, dataProvider = "testUrls")
-	public void verifyMidrollOverlayUpNext(String testName, UrlObject url)
-			throws OoyalaException {
-
+	public void verifyMidrollOverlayUpNext(String testName, UrlObject url)throws OoyalaException {
 		boolean result = true;
-
 		try {
-
 			driver.get(url.getUrl());
 
 			result = result && playValidator.waitForPage();
@@ -38,19 +34,17 @@ public class PlaybackMidrollOverlayUpNextTests extends PlaybackWebTest {
 
 			result = result && playValidator.validate("playing_1", 1000);
 
-			result = result
-					&& event.validate("MidRoll_willPlaySingleAd_1", 20000);
+			result = result && event.validate("MidRoll_willPlaySingleAd_1", 20000);
 
 			result = result && event.validate("singleAdPlayed_1", 10000);
 
 			result = result && event.validate("showNonlinearAd_1", 60000);
 
-			result = result
-					&& overLayValidator.validate("nonlinearAdPlayed_1", 2000);
+			result = result && overlayValidator.validate("nonlinearAdPlayed_1", 6000);
 
-			result = result
-					&& discoverValidator.validate("reportDiscoveryClick_1",
-							60000);
+			result = result && overlayValidator.validateOverlayRenderingEvent(6000);
+
+			result = result && discoverValidator.validate("reportDiscoveryClick_1",60000);
 
 		} catch (Exception e) {
 			e.printStackTrace();
