6f4d493c9db4b763ae5ecc4b1cd91cdf9f440d7b
Suraj Jogdand <suraj@Surajs-Mac-mini.local>
2017-04-25 19:22:46 +0530

    done proper indetation and refactoring

--- src/test/java/com/ooyala/playback/amf/postroll/PlaybackPostRollPoddedAdsTests.java
+++ src/test/java/com/ooyala/playback/amf/postroll/PlaybackPostRollPoddedAdsTests.java
@@ -11,47 +11,34 @@ import com.ooyala.qe.common.exception.OoyalaException;
 
 public class PlaybackPostRollPoddedAdsTests extends PlaybackWebTest {
 
-	public PlaybackPostRollPoddedAdsTests() throws OoyalaException {
-		super();
-	}
+    public PlaybackPostRollPoddedAdsTests() throws OoyalaException {
+        super();
+    }
 
-	private EventValidator event;
-	private PlayValidator playValidator;
-	private SeekValidator seekValidator;
-	private PoddedAdValidator poddedAdValidator;
-	private SetEmbedCodeValidator setEmbedCodeValidator;
+    private EventValidator event;
+    private PlayValidator playValidator;
+    private SeekValidator seekValidator;
+    private PoddedAdValidator poddedAdValidator;
+    private SetEmbedCodeValidator setEmbedCodeValidator;
 
-	@Test(groups = { "amf", "postroll", "podded" }, dataProvider = "testUrls")
-	public void verifyPostrollPodded(String testName, UrlObject url) throws OoyalaException {
-
-		boolean result = true;
-
-		try {
-
-			driver.get(url.getUrl());
-
-			result = result && playValidator.waitForPage();
-
-			injectScript();
-
-			result = result && playValidator.validate("playing_1", 60000);
-			result = result && seekValidator.validate("seeked_1", 60000);
-			result = result && event.validate("played_1", 60000);
-
-			result = result && poddedAdValidator.setPosition("PostRoll").validate("countPoddedAds", 10000);
-
-			if(testName.contains("SetEmbedCode")){
-				result = result && setEmbedCodeValidator.validate("",6000);
-			}
-
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			result = false;
-		}
-
-		Assert.assertTrue(result, "Tests failed");
-
-	}
-
+    @Test(groups = {"amf", "postroll", "podded"}, dataProvider = "testUrls")
+    public void verifyPostrollPodded(String testName, UrlObject url) throws OoyalaException {
+        boolean result = true;
+        try {
+            driver.get(url.getUrl());
+            result = result && playValidator.waitForPage();
+            injectScript();
+            result = result && playValidator.validate("playing_1", 60000);
+            result = result && seekValidator.validate("seeked_1", 60000);
+            result = result && event.validate("played_1", 60000);
+            result = result && poddedAdValidator.setPosition("PostRoll").validate("countPoddedAds", 10000);
+            if (testName.contains("SetEmbedCode")) {
+                result = result && setEmbedCodeValidator.validate("", 6000);
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result = false;
+        }
+        Assert.assertTrue(result, "Tests failed");
+    }
 }
