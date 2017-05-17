cbe2c701cde58ba132d87a08a1c0475130b10435
Suraj Jogdand <suraj@Surajs-Mac-mini.local>
2017-05-17 19:45:29 +0530

    refactored validatores for safari browser

--- src/main/java/com/ooyala/playback/page/action/PlayAction.java
+++ src/main/java/com/ooyala/playback/page/action/PlayAction.java
@@ -1,6 +1,8 @@
 package com.ooyala.playback.page.action;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 import com.ooyala.playback.page.PlayBackPage;
@@ -8,6 +10,7 @@ import com.relevantcodes.extentreports.LogStatus;
 
 public class PlayAction extends PlayBackPage implements PlayerAction {
 
+    public static Logger logger = Logger.getLogger(PlayAction.class);
     public PlayAction(WebDriver webDriver) {
         super(webDriver);
         PageFactory.initElements(webDriver, this);
@@ -39,6 +42,10 @@ public class PlayAction extends PlayBackPage implements PlayerAction {
                 extentTest.log(LogStatus.FAIL, "Loading spinner seems to be there for a really long time.");
                 return false;
             }
+            if (getBrowser().contains("safari")) {
+                WebElement element1 = getWebElement("PLAY_BUTTON");
+                driver.executeScript("arguments[0].click()",element1);
+            }
         }
         return true;
     }
@@ -53,7 +60,7 @@ public class PlayAction extends PlayBackPage implements PlayerAction {
             if (!clickOnIndependentElement("STATE_SCREEN_SELECTABLE"))
                 return false;
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error(e.getMessage());
             moveElement(getWebElement("PLAY_BUTTON"));
             Thread.sleep(5000);
             return startAction();
