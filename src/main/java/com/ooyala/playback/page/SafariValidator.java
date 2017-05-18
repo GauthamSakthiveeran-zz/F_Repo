a6841bf56e08b7520b9d8b3f108a949f0021a2d7
Suraj Jogdand <suraj@Surajs-Mac-mini.local>
2017-05-18 21:03:22 +0530

    added new validator for safari

---
+++ src/main/java/com/ooyala/playback/page/SafariValidator.java
@@ -0,0 +1,39 @@
+package com.ooyala.playback.page;
+
+import com.ooyala.playback.factory.PlayBackFactory;
+import com.relevantcodes.extentreports.LogStatus;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by suraj on 5/18/17.
+ */
+public class SafariValidator extends PlayBackPage implements PlaybackValidator {
+
+    public static Logger logger = Logger.getLogger(SafariValidator.class);
+
+    public SafariValidator(WebDriver webDriver) {
+        super(webDriver);
+        PageFactory.initElements(webDriver, this);
+        /**
+         * Here we will tell Facile to add the page elements of our Login Page
+         */
+        addElementToPageElements("discovery");
+        addElementToPageElements("play");
+        addElementToPageElements("pause");
+    }
+
+    public boolean validate(String element, int timeout) throws Exception {
+        try {
+            WebElement element1 = getWebElement(element);
+            driver.executeScript("arguments[0].click()", element1);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+}
