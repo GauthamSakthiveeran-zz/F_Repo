dde6848c5393be7f510db62d393ed6314d1642cb
jitendra pisal <jitendra.pisal@forgeahead.io>
2017-07-07 18:25:54 +0530

    added new test scripts in api for ads

---
+++ src/test/java/com/ooyala/playback/api/CheckAPIForMidrollAdTests.java
@@ -0,0 +1,41 @@
+package com.ooyala.playback.api;
+
+import com.ooyala.playback.PlaybackWebTest;
+import com.ooyala.playback.page.OoyalaAPIValidator;
+import com.ooyala.playback.page.PlayValidator;
+import com.ooyala.playback.page.action.PlayAction;
+import com.ooyala.playback.url.UrlObject;
+import com.ooyala.qe.common.exception.OoyalaException;
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by jitendra on 7/7/17.
+ */
+public class CheckAPIForMidrollAdTests extends PlaybackWebTest {
+
+    private PlayValidator play;
+    private PlayAction playAction;
+    private OoyalaAPIValidator api;
+
+    public CheckAPIForMidrollAdTests() throws OoyalaException {
+        super();
+    }
+
+    @Test(groups = "api",dataProvider = "testUrls")
+    public void testApiForMidrollAd(String testName, UrlObject url){
+        boolean result = true;
+        try {
+            driver.get(url.getUrl());
+            result = result && play.waitForPage();
+            injectScript();
+            result = result && api.validateAPIForMidroll();
+
+        }catch (Exception ex){
+            ex.getStackTrace();
+            result = false;
+        }
+
+        Assert.assertTrue(result);
+    }
+}
