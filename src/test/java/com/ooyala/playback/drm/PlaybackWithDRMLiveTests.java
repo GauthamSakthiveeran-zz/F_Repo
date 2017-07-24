4e687ab290a0de7cabc65946ab757ff047cc1134
Divya Manohar <dmanohar@ooyala.com>
2017-07-24 15:58:15 +0530

    video play initial play time.

--- src/test/java/com/ooyala/playback/drm/PlaybackWithDRMLiveTests.java
+++ src/test/java/com/ooyala/playback/drm/PlaybackWithDRMLiveTests.java
@@ -11,8 +11,6 @@ import com.ooyala.playback.page.EventValidator;
 import com.ooyala.playback.page.LiveValidator;
 import com.ooyala.playback.page.PauseValidator;
 import com.ooyala.playback.page.PlayValidator;
-import com.ooyala.playback.page.SeekValidator;
-import com.ooyala.playback.page.action.SeekAction;
 import com.ooyala.playback.url.UrlObject;
 import com.ooyala.qe.common.exception.OoyalaException;
 
@@ -21,8 +19,6 @@ public class PlaybackWithDRMLiveTests extends PlaybackWebTest {
 	private EventValidator eventValidator;
 	private PlayValidator play;
 	private PauseValidator pause;
-	private SeekValidator seek;
-	private SeekAction seekAction;
 	private DRMValidator drm;
     private LiveValidator live;
     private ErrorDescriptionValidator error;
@@ -60,21 +56,10 @@ public class PlaybackWithDRMLiveTests extends PlaybackWebTest {
 			
 			result = result && play.validate("playing_1", 60000);
 			
-			result = result && eventValidator.playVideoForSometime(3);
-
 			result = result && pause.validate("paused_1", 60000);
 
 			result = result && play.validate("playing_2", 60000);
 
-			/*if (!(testName.split("-")[1].trim()
-					.equalsIgnoreCase("elemental fairplay fairplay hls + opt")))
-				result = result && seek.validate("seeked_1", 60000);
-			else{
-				result = result && seekAction.fromLast().setTime(2).startAction();
-				result = result && eventValidator.validate("seeked_1", 60000);
-			}
-
-			result = result && eventValidator.validate("played_1", 60000);*/
 
             if (isChannelIdPresent) {
 
