e8f23570b822f34ab386b6707101af94f0b8c367
Divya Manohar <dmanohar@ooyala.com>
2017-08-24 11:21:11 +0530

    Edge and safari also in parallel

--- src/test/java/com/ooyala/playback/PlaybackWebTest.java
+++ src/test/java/com/ooyala/playback/PlaybackWebTest.java
@@ -212,8 +212,8 @@ public abstract class PlaybackWebTest extends FacileTest {
             if (browser == null || browser.equals(""))
                 browser = "firefox";
             logger.info("browser is " + browser);
-            if (browser.equalsIgnoreCase("safari") || browser.toLowerCase().contains("edge") /*|| browser.toLowerCase().contains("internet") || browser.equalsIgnoreCase("ie")*/
-                    || System.getProperty(CommandLineParameters.platform).equalsIgnoreCase("android")) {
+            if (/*browser.equalsIgnoreCase("safari") || browser.toLowerCase().contains("edge") || browser.toLowerCase().contains("internet") || browser.equalsIgnoreCase("ie")
+                    || */System.getProperty(CommandLineParameters.platform).equalsIgnoreCase("android")) {
                 // safari driver does not allow parallel execution of tests
                 // ie because the browser hangs - this is a temp soln.
                 context.getCurrentXmlTest().setParallel("false");
