895b10ce2ca55fa0d162cca39926191d4bd75567
jitendra pisal <jitendra.pisal@forgeahead.io>
2017-08-24 18:31:12 +0530

    added new classes for page level overriding

---
+++ src/test/java/com/ooyala/playback/amf/vast/pageleveloverride/PlaybackMidrollPageLevelOverrideTests.java
@@ -0,0 +1,47 @@
+package com.ooyala.playback.amf.vast.pageleveloverride;
+
+import com.ooyala.playback.PlaybackWebTest;
+import com.ooyala.playback.page.EventValidator;
+import com.ooyala.playback.page.PageLevelOverridingValidator;
+import com.ooyala.playback.page.PlayValidator;
+import com.ooyala.playback.page.action.PlayAction;
+import com.ooyala.playback.url.UrlObject;
+import com.ooyala.qe.common.exception.OoyalaException;
+import com.relevantcodes.extentreports.LogStatus;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Jitendra on 8/23/17.
+ */
+
+public class PlaybackMidrollPageLevelOverrideTests extends PlaybackWebTest{
+    public PlaybackMidrollPageLevelOverrideTests() throws OoyalaException {
+        super();
+    }
+    private PlayValidator playValidator;
+    private PageLevelOverridingValidator pageOverride;
+    private PlayAction playAction;
+    private EventValidator event;
+
+    @Test(dataProvider = "testUrls",groups = "pageLevelOverriding")
+    public void midrollPageLevelOverride(String testName, UrlObject url){
+        boolean result= true;
+        try {
+            driver.get(url.getUrl());
+            result = result && playValidator.waitForPage();
+            injectScript();
+            result = result && playAction.startAction();
+            result = result && event.validate("playing_1", 60000);
+            result = result && event.validate("MidRoll_willPlaySingleAd_1", 150000);
+            result = result && event.validate("singleAdPlayed_1", 120000);
+            result = result && event.validate("playing_2",10000);
+            result = result && pageOverride.setAdType("Midroll").validate("",1000);
+
+        }catch (Exception e){
+            extentTest.log(LogStatus.FAIL,e.getMessage());
+            result = false;
+        }
+
+        org.testng.Assert.assertTrue(result, "Midroll Page Level Overriding tests failed");
+    }
+}
---
+++ src/test/java/com/ooyala/playback/amf/vast/pageleveloverride/PlaybackPostrollPageLevelOverrideTests.java
@@ -0,0 +1,51 @@
+package com.ooyala.playback.amf.vast.pageleveloverride;
+
+import com.ooyala.playback.PlaybackWebTest;
+import com.ooyala.playback.page.EventValidator;
+import com.ooyala.playback.page.PageLevelOverridingValidator;
+import com.ooyala.playback.page.PlayValidator;
+import com.ooyala.playback.page.SeekValidator;
+import com.ooyala.playback.page.action.PlayAction;
+import com.ooyala.playback.url.UrlObject;
+import com.ooyala.qe.common.exception.OoyalaException;
+import com.relevantcodes.extentreports.LogStatus;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Jitendra on 8/23/17.
+ */
+
+public class PlaybackPostrollPageLevelOverrideTests extends PlaybackWebTest{
+    public PlaybackPostrollPageLevelOverrideTests() throws OoyalaException {
+        super();
+    }
+    private PlayValidator playValidator;
+    private PageLevelOverridingValidator pageOverride;
+    private PlayAction playAction;
+    private EventValidator event;
+    private SeekValidator seek;
+
+    @Test(dataProvider = "testUrls",groups = "pageLevelOverriding")
+    public void postrollPageLevelOverride(String testName, UrlObject url){
+        boolean result= true;
+        try {
+            driver.get(url.getUrl());
+            result = result && playValidator.waitForPage();
+            injectScript();
+            result = result && playAction.startAction();
+            result = result && event.validate("playing_1", 60000);
+            result = result && event.playVideoForSometime(3);
+            driver.executeScript("pp.seek(pp.getDuration()-10)");
+            result = result && event.validate("PostRoll_willPlaySingleAd_1", 60000);
+            result = result && event.validate("singleAdPlayed_1", 120000);
+            result = result && event.validate("played_1", 10000);
+            result = result && pageOverride.setAdType("Postroll").validate("",1000);
+
+        }catch (Exception e){
+            extentTest.log(LogStatus.FAIL,e.getMessage());
+            result = false;
+        }
+
+        org.testng.Assert.assertTrue(result, "Postroll Page Level Overriding tests failed");
+    }
+}
