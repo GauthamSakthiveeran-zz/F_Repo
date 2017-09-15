440ea0edb8bb7ad272f99d1aa40b5ed52d6e12b6
Suraj Jogdand <suraj@Surajs-Mac-mini.local>
2017-09-15 15:19:03 +0530

    added test for preroll preloading

---
+++ src/test/java/com/ooyala/playback/preloading/PlaybackEnablePreloadingForPrerollTests.java
@@ -0,0 +1,52 @@
+package com.ooyala.playback.preloading;
+
+import com.ooyala.playback.PlaybackWebTest;
+import com.ooyala.playback.page.EventValidator;
+import com.ooyala.playback.page.PreloadingValidator;
+import com.ooyala.playback.page.action.PlayAction;
+import com.ooyala.playback.url.UrlObject;
+import com.ooyala.qe.common.exception.OoyalaException;
+import com.relevantcodes.extentreports.LogStatus;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by suraj on 9/14/17.
+ */
+public class PlaybackEnablePreloadingForPrerollTests extends PlaybackWebTest {
+    public PlaybackEnablePreloadingForPrerollTests() throws OoyalaException {
+    }
+
+    private static final Logger logger = Logger.getLogger(PlaybackEnablePreloadingForPrerollTests.class);
+    private EventValidator eventValidator;
+    private PlayAction playAction;
+    private PreloadingValidator preloadingValidator;
+
+    @Test(dataProvider = "testUrls")
+    public void enablePreloadingForPrerollTest(String testName, UrlObject url) throws OoyalaException {
+        boolean result = true;
+        try {
+            driver.get(url.getUrl());
+            result = result && eventValidator.isPageLoaded();
+            injectScript();
+            boolean isAdPlaying = false;
+            preloadingValidator.getConsoleLogs();
+            result = result && playAction.startAction();
+            result = result && preloadingValidator.validate("firstQuartile", 10000);
+            result = result && preloadingValidator.validateFalse("downloads", 1000);
+            result = result && preloadingValidator.validateFalse("segments",500);
+            result = result && preloadingValidator.validate("thirdQuartile", 10000);
+            result = result && preloadingValidator.validate("downloads",3000);
+            result = result && preloadingValidator.validate("segments", 2000);
+            result = result && eventValidator.validate("playing_1",5000);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            extentTest.log(LogStatus.FAIL, ex.getMessage());
+            logger.error(ex.getMessage());
+            result = false;
+        }
+        Assert.assertTrue(result, "Playback Enable Preloading For Preroll Ads Tests failed");
+    }
+}
