35b8cf733a53a10b3d3b65159f2ecd9f449679ed
“Inna <ilebedeva@ooyala.com>
2017-10-03 13:36:54 +0300

    resolve merge conflict

--- src/main/java/com/ooyala/playback/page/action/VirtualRealityAction.java
+++ src/main/java/com/ooyala/playback/page/action/VirtualRealityAction.java
@@ -100,7 +100,7 @@ public class VirtualRealityAction extends PlayBackPage implements PlayerAction {
             Actions actions = new Actions(driver);
             WebElement screen = getWebElement("STATE_SCREEN_SELECTABLE");
             actions.moveToElement(screen).perform();
-            actions.sendKeys(keyButton);
+            actions.sendKeys(keyButton).perform();
             actions.build().perform();
 
             return true;
