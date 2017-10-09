ff74885f4ac323a0454c078f2f6161e248469fac
Suraj Jogdand <suraj@Surajs-Mac-mini.local>
2017-10-09 17:59:12 +0530

    Added test for dynamic filters

---
+++ src/test/java/com/ooyala/playback/analytics/AnalyticsPlaylistTests.java
@@ -0,0 +1,14 @@
+package com.ooyala.playback.analytics;
+
+import com.ooyala.playback.PlaybackWebTest;
+import com.ooyala.qe.common.exception.OoyalaException;
+
+/**
+ * Created by suraj on 9/29/17.
+ */
+public class AnalyticsPlaylistTests extends PlaybackWebTest {
+    public AnalyticsPlaylistTests() throws OoyalaException {
+    }
+
+
+}
