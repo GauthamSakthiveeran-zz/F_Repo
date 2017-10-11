5b56edfdacabb81fb50880dcd51f09c79e1051bf
gsakthiveeeran <gsakthiveeeran@ooyala.com>
2017-10-11 11:53:47 +0530

    Changes to notification for android

--- src/test/java/com/ooyala/playback/apps/android/ooyalaskinsamplespp/OoyalaSkinSampleAppUtils.java
+++ src/test/java/com/ooyala/playback/apps/android/ooyalaskinsamplespp/OoyalaSkinSampleAppUtils.java
@@ -73,7 +73,7 @@ public class OoyalaSkinSampleAppUtils extends PlaybackAppsTest {
 				result = result && notificationEventValidator.validateEvent(Events.AD_COMPLETED, 20000);
 			}
 			result = result && clickDiscoveryAction.clickPauseButton();
-			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED, 70000);
+			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED_ANDRD, 70000);
 			result = result && clickDiscoveryAction.seekForward("SEEKBAR_ANDROID");
 			result = result && notificationEventValidator.validateEvent(Events.SEEK_STARTED, 20000);
 			result = result && notificationEventValidator.validateEvent(Events.SEEK_COMPLETED, 20000);
@@ -121,7 +121,7 @@ public class OoyalaSkinSampleAppUtils extends PlaybackAppsTest {
 			}
 			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_STARTED, 20000);
 			result = result && clickDiscoveryAction.clickPauseButton();
-			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED, 70000);
+			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED_ANDRD, 70000);
 			result = result && clickDiscoveryAction.seekForward("SEEKBAR_ANDROID");
 			result = result && notificationEventValidator.validateEvent(Events.SEEK_STARTED, 20000);
 			result = result && notificationEventValidator.validateEvent(Events.SEEK_COMPLETED, 20000);
@@ -169,7 +169,7 @@ public class OoyalaSkinSampleAppUtils extends PlaybackAppsTest {
 			}
 			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_STARTED, 20000);
 			result = result && clickDiscoveryAction.clickPauseButton();
-			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED, 70000);
+			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED_ANDRD, 70000);
 			result = result && clickDiscoveryAction.seekForward("SEEKBAR_ANDROID");
 			result = result && notificationEventValidator.validateEvent(Events.SEEK_STARTED, 20000);
 			result = result && notificationEventValidator.validateEvent(Events.SEEK_COMPLETED, 20000);
@@ -226,7 +226,7 @@ public class OoyalaSkinSampleAppUtils extends PlaybackAppsTest {
 			result = result && notificationEventValidator.validateEvent(Events.AD_SKIPPPED, 20000);
 			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_STARTED, 20000);
 			result = result && clickDiscoveryAction.clickPauseButton();
-			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED, 70000);
+			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED_ANDRD, 70000);
 			result = result && clickDiscoveryAction.seekForward("SEEKBAR_ANDROID");
 			result = result && notificationEventValidator.validateEvent(Events.SEEK_STARTED, 20000);
 			result = result && notificationEventValidator.validateEvent(Events.SEEK_COMPLETED, 20000);
@@ -279,7 +279,7 @@ public class OoyalaSkinSampleAppUtils extends PlaybackAppsTest {
 			}
 			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_STARTED, 20000);
 			result = result && clickDiscoveryAction.clickPauseButton();
-			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED, 70000);
+			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED_ANDRD, 70000);
 			result = result && clickDiscoveryAction.seekForward("SEEKBAR_ANDROID");
 			result = result && notificationEventValidator.validateEvent(Events.SEEK_STARTED, 20000);
 			result = result && notificationEventValidator.validateEvent(Events.SEEK_COMPLETED, 20000);
@@ -405,7 +405,7 @@ public class OoyalaSkinSampleAppUtils extends PlaybackAppsTest {
 				result = result && notificationEventValidator.validateEvent(Events.AD_COMPLETED, 20000);
 			}
 			result = result && clickDiscoveryAction.clickPauseButton();
-			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED, 70000);
+			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED_ANDRD, 70000);
 			result = result && clickDiscoveryAction.seekToEnd("SEEKBAR_ANDROID");
 			result = result && elementValidator.handleLoadingSpinner();
 			result = result && notificationEventValidator.validateEvent(Events.SEEK_STARTED, 20000);
@@ -448,7 +448,7 @@ public class OoyalaSkinSampleAppUtils extends PlaybackAppsTest {
 				result = result && notificationEventValidator.validateEvent(Events.AD_COMPLETED, 30000);
 			}
 			result = result && clickDiscoveryAction.clickPauseButton();
-			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED, 70000);
+			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED_ANDRD, 70000);
 			result = result && clickDiscoveryAction.seekToEnd("SEEKBAR_ANDROID");
 			result = result && elementValidator.handleLoadingSpinner();
 			result = result && notificationEventValidator.validateEvent(Events.SEEK_STARTED, 20000);
@@ -488,7 +488,7 @@ public class OoyalaSkinSampleAppUtils extends PlaybackAppsTest {
 				result = result && notificationEventValidator.validateEvent(Events.AD_COMPLETED, 20000);
 			}
 			result = result && clickDiscoveryAction.clickPauseButton();
-			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED, 70000);
+			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED_ANDRD, 70000);
 			result = result && clickDiscoveryAction.seekToEnd("SEEKBAR_ANDROID");
 			result = result && elementValidator.handleLoadingSpinner();
 			result = result && notificationEventValidator.validateEvent(Events.SEEK_STARTED, 20000);
@@ -526,7 +526,7 @@ public class OoyalaSkinSampleAppUtils extends PlaybackAppsTest {
 			result = result && notificationEventValidator.validateEvent(Events.AD_COMPLETED, 20000);
 			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_STARTED, 20000);
 			result = result && clickDiscoveryAction.clickPauseButton();
-			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED, 70000);
+			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED_ANDRD, 70000);
 			result = result && clickDiscoveryAction.seekToEnd("SEEKBAR_ANDROID");
 			result = result && elementValidator.handleLoadingSpinner();
 			result = result && notificationEventValidator.validateEvent(Events.SEEK_STARTED, 20000);
@@ -559,7 +559,7 @@ public class OoyalaSkinSampleAppUtils extends PlaybackAppsTest {
 			result = result && notificationEventValidator.validateEvent(Events.AD_COMPLETED, 35000);
 			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_STARTED, 20000);
 			result = result && clickDiscoveryAction.clickPauseButton();
-			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED, 70000);
+			result = result && notificationEventValidator.validateEvent(Events.PLAYBACK_PAUSED_ANDRD, 70000);
 			result = result && clickDiscoveryAction.seekToEnd("SEEKBAR_ANDROID");
 			result = result && elementValidator.handleLoadingSpinner();
 			result = result && notificationEventValidator.validateEvent(Events.SEEK_STARTED, 20000);
