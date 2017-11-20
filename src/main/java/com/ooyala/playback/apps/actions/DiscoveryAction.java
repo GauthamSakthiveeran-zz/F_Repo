09bc7c03fbb1aef539ed44fad8fc2c65152910f7
Shivam Gupta <shivam.gupta@forgeahead.io>
2017-11-20 17:02:35 +0530

    renamed the name of the element, removed latest

--- src/main/java/com/ooyala/playback/apps/actions/DiscoveryAction.java
+++ src/main/java/com/ooyala/playback/apps/actions/DiscoveryAction.java
@@ -202,7 +202,7 @@ public class DiscoveryAction extends PlaybackApps implements Actions {
     	{
  
     	Thread.sleep(3000);
-		WebElement playPauseButton = getWebElement("PLAY_PAUSE_BUTTON_ANDROID_LATEST");
+		WebElement playPauseButton = getWebElement("PLAY_PAUSE_BUTTON_ANDROID");
 		p[0] = playPauseButton.getLocation().getX() + playPauseButton.getSize().getWidth()/2;
 		p[1] = playPauseButton.getLocation().getY() + playPauseButton.getSize().getHeight()/2;
         TouchAction touch = new TouchAction((AppiumDriver) driver);
